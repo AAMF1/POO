bfba6df13a0e3b378809502c44152ab8339fe8c5
AAMF1 <alex.amf17@gmail.com>
2020-07-08 02:08:36 +0100

    First commit

--- src/projeto/Aluno.java
+++ src/projeto/Aluno.java
@@ -2,26 +2,31 @@ package projeto;
 
 import java.io.Serializable;
 
-/** Classe que extende da classe Pessoa e que cria o objeto aluno
+/**
+ * Classe que extende da classe Pessoa e que cria o objeto aluno
  */
-public class Aluno extends Pessoa implements Serializable{
+public class Aluno extends Pessoa implements Serializable {
     private String curso;
-    
-    /** Construtor para a criação de um objeto do tipo aluno
-     * @param curso curso do aluno 
-     * @param nome nome do aluno
+
+    /**
+     * Construtor para a criação de um objeto do tipo aluno
+     * 
+     * @param curso  curso do aluno
+     * @param nome   nome do aluno
      * @param perfil perfil do aluno (Boemio, Poupadinho, Cultural ou Desportivo)
      */
     public Aluno(String curso, String nome, String perfil) {
         super(nome, perfil);
         this.curso = curso;
     }
-    
-    /** Método para retornar curso
+
+    /**
+     * Método para retornar curso
+     * 
      * @return String - curso
      */
     @Override
-    public String getCurso(){
+    public String getCurso() {
         return curso;
     }
 }
